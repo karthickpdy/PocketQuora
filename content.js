5a5be8169571c0d5962c8c2b97e6e323b8443683
Vignesh Jayavel <vigneshjayavel@gmail.com>
2014-09-06 16:20:21 +0530

    added pocket button for answer

--- content.js
+++ content.js
@@ -1,4 +1,19 @@
-console.log("content.js loaded")
+document.body.onload = function() {
+	console.log("content.js loaded")
+
+	//add button to all answers present while content.js is loaded
+	$(".AnswerActionBar").append("<a href=#><span class='pocket_button'>pocket</span></a>")
+
+	$("span.pocket_button").on("click", function(e){
+			console.log("get_answer_link")
+			var parent = $(this).parents(".Answer")
+			var link = parent.find("div.ContentFooter span:first a").attr("href");
+			var fullLink = "https://quora.com"+link
+			console.log(fullLink)
+	})
+
+}
+
 
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.type) {
